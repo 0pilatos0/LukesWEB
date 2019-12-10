fa90af16161ddedeea80bbadab6e992d8b2b6bcc
pilatos <paulvanderlei@hotmail.nl>
2019-12-10 22:17:03 +0100

    kleine update voor slapengaan

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-function changeImage(){
+function changeImage() {
 
+    if (document.getElementById("afbeelding").src == "http://www.userinterfaceicons.com/80x80/minimize.png") 
+    {
+        document.getElementById("afbeelding").src = "http://www.userinterfaceicons.com/80x80/maximize.png";
+    }
+    else 
+    {
+        document.getElementById("afbeelding").src = "http://www.userinterfaceicons.com/80x80/minimize.png";
+    }
 }
