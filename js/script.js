7093bfb5a002f45d67878cfb1314a4feb3c0adfb
pilatos <paulvanderlei@hotmail.nl>
2019-12-11 18:02:15 +0100

    aangevraagde fix gedaan

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 function changeImage() {
         document.getElementById("afbeelding").src = "./images/agenda.png";
+        document.getElementById("agenda").src = "./images/agendalogo.png";
+        document.getElementById("scanner").src = "./images/scannerlogo.png";
         console.log("test2")
 }
 function changeImage1() {
         document.getElementById("afbeelding").src = "./images/camera.png";
+        document.getElementById("scanner").src = "./images/scannerlogo2.png";
+        document.getElementById("agenda").src = "./images/agendalogo2.png";
         console.log("test")
 }
